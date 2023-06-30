a30334c98a395304dcd546b01ebeb623bffde854
OEM <gurleva_ludmila@mail.ru>
2023-06-30 19:27:50 +0300

    Change files

--- src/test/java/ru/netology/services/VacationRestServiceTest.java
+++ src/test/java/ru/netology/services/VacationRestServiceTest.java
@@ -1,29 +1,18 @@
 package ru.netology.services;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-//import ru.netology.services.VacationRestService;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
 
 public class VacationRestServiceTest {
-    @Test
-    public void VacationRestCalc() {
+    @ParameterizedTest
+    @CsvFileSource(files= "src/test/resources/bases.csv")
+    public void VacationRestCalcWithParams() {
         VacationRestService service = new VacationRestService();
-
         //  данные:
         int actual = service.calculate(10000,3000,20000);
         int expected= 3;
-
-        // производим проверку (сравниваем ожидаемый и фактический):
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    public void VacationRestCalc2() {
-        VacationRestService service = new VacationRestService();
-
-        // данные
-        int actual = service.calculate(100000,60000,150000);
-        int expected = 2;
 
         // производим проверку (сравниваем ожидаемый и фактический):
         Assertions.assertEquals(expected, actual);
